bec0e89e3a6edc1c2144145e793e898285690a44
Paulo Castellano <paulofcastellano@gmail.com>
2024-05-19 20:19:55 -0300

    improvements at vite

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -4,11 +4,11 @@ export const devConfig = {
   sdkKey: '',
   sdkSecret: '',
   webEndpoint: 'zoom.us',
-  topic: 'Daily meeting', // meeting name
+  topic: 'DailyMeeting', // meeting name
   name: `${getExploreName()}-${Math.floor(Math.random() * 1000)}`, // user name
   password: '',
   signature: '',
-  sessionKey: '9c14e63b-0fe3-4a09-aaec-c75831a67e8f', // meeting id
+  sessionKey: '', // meeting id
   userIdentity: '',
   role: 1
 };
