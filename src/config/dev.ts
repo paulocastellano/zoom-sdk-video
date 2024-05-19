e0255eb29172054ce33667a8fbfb11cfe6d7d025
Paulo Castellano <paulofcastellano@gmail.com>
2024-05-19 18:13:38 -0300

    first commit

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -1,15 +1,16 @@
 import { getExploreName } from '../utils/platform';
+import { v4 as uuidv4 } from 'uuid';
 
 export const devConfig = {
   sdkKey: '',
   sdkSecret: '',
   webEndpoint: 'zoom.us',
-  topic: '20 may daily', // meeting name
+  topic: 'Daily meeting', // meeting name
   name: `${getExploreName()}-${Math.floor(Math.random() * 1000)}`, // user name
   password: '',
   signature:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoibVIwSmJJLUlTNVdEbGNPdjVXRmZmZyIsInJvbGVfdHlwZSI6MSwidHBjIjoiMjAgbWF5IGRhaWx5IiwidmVyc2lvbiI6MSwiaWF0IjoxNzE2MTUyMzY0LCJleHAiOjE3MTYxNTU5NjR9.iCYblH6vSKeka7R2TDZuw7x6X_pd8oGp9ezIBHaXHsA',
-  sessionKey: '', // meeting id
-  userIdentity: '', // user id
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoibVIwSmJJLUlTNVdEbGNPdjVXRmZmZyIsInJvbGVfdHlwZSI6MSwidHBjIjoiRGFpbHkgbWVldGluZyIsInZlcnNpb24iOjEsImlhdCI6MTcxNjE1MzE4OCwiZXhwIjoxNzE2MTU2Nzg4fQ.p46n0_Zy3FWH_WA7-AOwkWGKOVDjRnfV65_YNpAuZXo',
+  sessionKey: '9c14e63b-0fe3-4a09-aaec-c75831a67e8f', // meeting id
+  userIdentity: uuidv4(),
   role: 1
 };
