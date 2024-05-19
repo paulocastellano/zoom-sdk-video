1e4f83f0c78eab97359b2bd73e8d8a65295325fe
Paulo Castellano <paulofcastellano@gmail.com>
2024-05-19 18:04:06 -0300

    first commit

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -10,6 +10,6 @@ export const devConfig = {
   signature:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoibVIwSmJJLUlTNVdEbGNPdjVXRmZmZyIsInJvbGVfdHlwZSI6MSwidHBjIjoiMjAgbWF5IGRhaWx5IiwidmVyc2lvbiI6MSwiaWF0IjoxNzE2MTUyMzY0LCJleHAiOjE3MTYxNTU5NjR9.iCYblH6vSKeka7R2TDZuw7x6X_pd8oGp9ezIBHaXHsA',
   sessionKey: '', // meeting id
-  userIdentity: '1', // user id
+  userIdentity: '', // user id
   role: 1
 };
